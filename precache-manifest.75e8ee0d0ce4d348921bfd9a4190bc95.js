826312eaf6567ec47608935515fb5c0498276c82
Vincent Rohde <vincent.rohde@icloud.com>
2018-10-06 21:21:23 +0200

    Updates

---
+++ precache-manifest.75e8ee0d0ce4d348921bfd9a4190bc95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8fefe6c8fd3ce2d862",
+    "url": "/indecision/static/js/runtime~main.cf8fefe6.js"
+  },
+  {
+    "revision": "3b3a7935f9758465a128",
+    "url": "/indecision/static/js/main.3b3a7935.chunk.js"
+  },
+  {
+    "revision": "ed4dc564f458e8c504f5",
+    "url": "/indecision/static/js/1.ed4dc564.chunk.js"
+  },
+  {
+    "revision": "3b3a7935f9758465a128",
+    "url": "/indecision/static/css/main.d3f297e8.chunk.css"
+  },
+  {
+    "revision": "7dd7630a62acee4b1b3cd6803b068ada",
+    "url": "/indecision/index.html"
+  }
+];
